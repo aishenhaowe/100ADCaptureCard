91ffec48cfd6476ccb1fec242910d9f179c5d7ec
break <break.chen@deltaww.com.cn>
2013-12-03 19:46:47 +0800

    将基板状态检测端口设为GPIO_Mode_IPD

--- BaseBoard/HARDWARE/BoardOnline/boardonline.c
+++ BaseBoard/HARDWARE/BoardOnline/boardonline.c
@@ -9,6 +9,6 @@ void OnlineKey_Init(void)
 	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | 
 								   GPIO_Pin_11| GPIO_Pin_12| GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
